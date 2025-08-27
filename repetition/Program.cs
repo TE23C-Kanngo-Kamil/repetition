1ee3cc05a7042a273162adc343cf325183a14c07
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-08-27 14:35:11 +0200

    kritiska träffar & missar

--- repetition/Program.cs
+++ repetition/Program.cs
@@ -2,40 +2,71 @@
 
 static void FightSim()
 {
-int p1Hp = 100;
-int p2Hp = 75;
+    int p1Hp = 100;
+    int p2Hp = 100;
 
-string p1Name = "David";
-string p2Name = "Goliath";
+    string p1Name = "David";
+    string p2Name = "Goliath";
 
-Console.WriteLine($"{p1Name} vs {p2Name} - FIGHT!");
+    Console.WriteLine($"{p1Name} vs {p2Name} - FIGHT!");
 
-while (p1Hp > 0 && p2Hp > 0)
-{
-    Console.WriteLine($"{p1Name}: {p1Hp}");
-    Console.WriteLine($"{p2Name}: {p2Hp}");
+    while (p1Hp > 0 && p2Hp > 0)
+    {
+        Console.WriteLine($"{p1Name}: {p1Hp}");
+        Console.WriteLine($"{p2Name}: {p2Hp}");
+        Console.ReadLine();
 
-    int p1Damage = Random.Shared.Next(25);
-    p2Hp -= p1Damage;
-    Console.WriteLine($"{p1Name} gör {p1Damage} på {p2Name}");
+        int p1Damage = Attack("David");
+        p2Hp -= p1Damage;
+        if (p1Damage > 0)
+        {
+            Console.WriteLine($"{p1Name} gör {p1Damage} skada på {p2Name}.");
+            Console.ReadLine();
+        }
 
-    int p2Damage = Random.Shared.Next(15);
-    p1Hp -= p2Damage;
-    Console.WriteLine($"{p2Name} gör {p2Damage} på {p1Name}");
+        int p2Damage = Attack("Goliath");
+        p1Hp -= p2Damage;
+        if (p1Damage > 0)
+        {
+            Console.WriteLine($"{p2Name} gör {p2Damage} skada på {p1Name}.");
+            Console.ReadLine();
+        }
+    }
     Console.ReadLine();
-}
 
-if (p2Hp <= 0 && p1Hp <= 0)
-{
-    Console.WriteLine($"{p1Name} och {p2Name} svimmade båda!");
-}
-else if (p1Hp <= 0)
-{
-    Console.WriteLine($"{p2Name} vann!");
-}
-else
-{
-    Console.WriteLine($"{p1Name} vann!");
-}
+
+    if (p2Hp <= 0 && p1Hp <= 0)
+    {
+        Console.WriteLine($"{p1Name} och {p2Name} svimmade båda!");
+    }
+    else if (p1Hp <= 0)
+    {
+        Console.WriteLine($"{p2Name} vann!");
+    }
+    else
+    {
+        Console.WriteLine($"{p1Name} vann!");
+    }
     Console.ReadLine();
+
+    static int Attack(string attacker)
+    {
+        int baseDamage = Random.Shared.Next(1, 20);
+        int roll = Random.Shared.Next(100);
+
+        if (roll < 20)
+        {
+            Console.WriteLine($"{attacker} missade attacken!");
+            Console.ReadLine();
+            return 0;
+        }
+        else if (roll < 50)
+        {
+            Console.WriteLine($"KRITISK TRÄFF av {attacker}!");
+            Console.ReadLine();
+            return baseDamage * 2;
+        }
+
+        return baseDamage;
+    }
 }
